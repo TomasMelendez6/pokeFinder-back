721089ca66255d464c6030e61ac4c3674d6df6e8
u900333 <TMelendez@proveedor.teco.com.ar>
2021-10-11 17:12:31 -0300

    Se agrega POO en el modelo Pokemon

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,16 +1,36 @@
 import model from '../models/pokemon.js'
 import msj from '../message/message.js'
+import axios from 'axios'
 
 export default {
     consultar: async (req, res) => {
         try{
-            const pokemons = await model.obtenerTodos()
-            if (pokemons.length > 0){
-                res.send(pokemons)
-            }else{
-                res.send(msj.mensajeSinResultados())
-            }          
-            
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=48');
+
+            if (response.statusCode !== undefined && response.statusCode !== 200) {
+                throw response;
+            } else {
+                const pokemons = response.data.results;
+
+                if (pokemons.length > 0){
+                    let result = []
+                    for (const pd of pokemons){
+                        const responseDetail = await axios.get(pd.url);
+                        if(responseDetail.statusCode !== undefined && responseDetail.statusCode !== 200){
+                            throw responseDetail;
+                        }
+                        else{
+                            let pokemonDetail = responseDetail.data
+                            let sprites = pokemonDetail.sprites
+                            let pokemon = new model.Pokemon(pokemonDetail.id, pokemonDetail.name, pokemonDetail.height, pokemonDetail.weight, sprites.front_default, sprites.back_default)
+                            result.push(pokemon.jsonConverter())
+                        }
+                    };
+                    res.send(result);
+                }else{
+                    res.send(msj.mensajeSinResultados())
+                }  
+            }
         }catch(error){
             msj.mensajeCustom(500, error)
         }
--- models/pokemon.js
+++ models/pokemon.js
@@ -1,20 +1,26 @@
-import axios from 'axios'
+class Pokemon {
+    constructor(id, name, height, weight, pic1, pic2){
+        this.id = id;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.pic1 = pic1;
+        this.pic2 = pic2;
+    }
 
-//Metodo para obtener todos los pokemones de la API externa
-async function obtenerTodos() {
-    try {
-        let response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=200')
-        // si hay error en la respuesta
-        if (response.statusCode !== undefined && response.statusCode !== 200) {
-            throw response
-        } else {
-            return response.data.results
+    jsonConverter(){
+        let json = {
+            id: this.id,
+            name: this.name,
+            height: this.height,
+            weight: this.weight,
+            pic1: this.pic1,
+            pic2: this.pic2
         }
-    } catch (error) {
-        throw error
+        return json
     }
 }
 
 export default {
-    obtenerTodos
-}
+        Pokemon
+    }
